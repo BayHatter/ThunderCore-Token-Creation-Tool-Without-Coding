0b685a567829a5d9efe8e216fbe3142ca5f2f386
Mohsin Khan <209888552+BayHatter@users.noreply.github.com>
2025-05-08 18:13:19 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>Ethereum Token Creation Tool</title>
-        <meta name="description" content="Create and launch your own meme token on Ethereum" />
-        <meta property="og:title" content="Launch Your Meme Coin on Ethereum" />
+        <title>ThunderCore Token Creation Tool</title>
+        <meta name="description" content="Create and launch your own meme token on ThunderCore" />
+        <meta property="og:title" content="Launch Your Meme Coin on ThunderCore" />
         <meta property="og:description" content="Build and deploy a meme coin instantly. Fast, secure." />
         <meta property="og:url" content="/" />
         <meta property="og:type" content="website" />
